74ca403d92ebced298a6462456e7d56738e6cf13
shaiju <shaiju@toobler.com>
2018-03-19 11:39:35 +0530

    chnages on hello

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis! \n');  // Test fails here!
-}).listen(8000, 'localhost');
+}).listen(80, 'finalsome3.herokuapp.com');
 console.log('Server running at http://shaijutest.herokuapp.com/');
