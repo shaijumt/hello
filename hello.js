e423f532fd805afe00ccf6977c4254cfea6c7140
shaiju <shaiju@toobler.com>
2018-03-19 13:44:57 +0530

    dddd

--- hello.js
+++ hello.js
@@ -1,6 +1,1 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis fif! \n');  // Test fails here!
-}).listen(8000, 'localhost');
-console.log('Server running at http://shaijutest.herokuapp.com/');
+console.log("SS");
